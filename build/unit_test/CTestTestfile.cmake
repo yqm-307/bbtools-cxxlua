03bb888f128e8560be507250a14e1f5e65443771
yqm-307 <979336542@qq.com>
2024-10-22 06:49:37 +0000

    删除 build误提交

--- build/unit_test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yqm/Git/owner/bbtools/bbtools-cxxlua/unit_test
-# Build directory: /home/yqm/Git/owner/bbtools/bbtools-cxxlua/build/unit_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test_luaclass_gc "/home/yqm/Git/owner/bbtools/bbtools-cxxlua/build/bin/unit_test/Test_luaclass_gc")
-set_tests_properties(Test_luaclass_gc PROPERTIES  _BACKTRACE_TRIPLES "/home/yqm/Git/owner/bbtools/bbtools-cxxlua/unit_test/CMakeLists.txt;15;add_test;/home/yqm/Git/owner/bbtools/bbtools-cxxlua/unit_test/CMakeLists.txt;0;")
